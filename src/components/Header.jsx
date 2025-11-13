4ceb77cf8626c083f99bbf922ae536e5a68b48fe
khushbu <pujakumari29122001@gmail.com>
2025-11-13 22:37:36 +0530

    style: add responsive CSS styling to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "./Header.css"; // Import CSS
 
 // Header component with navigation and cart count
 const Header = () => {
   const cartItems = useSelector((state) => state.cart.items);
 
   return (
-    <header className="header">
-      <h1><Link to="/">ShoppyGlobe</Link></h1>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/cart">Cart ({cartItems.length})</Link>
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <h1>
+          <Link to="/">ShoppyGlobe</Link>
+        </h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart ({cartItems.length})</Link>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
